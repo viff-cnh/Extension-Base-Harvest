b3b38f2109fa6e83d6b8ad1fd8253233fe5f5a73
bmarron18 <bmarron@pdx.edu>
2016-11-21 23:20:47 -0800

    Removed spurious reference comments in the headers (Marron)

--- src/InputParametersParser.cs
+++ src/InputParametersParser.cs
@@ -1,8 +1,3 @@
-// This file is part of the Base Harvest extension for LANDIS-II.
-// For copyright and licensing information, see the NOTICE and LICENSE
-// files in this project's top-level directory, and at:
-//   http://landis-extensions.googlecode.com/svn/trunk/base-harvest/trunk/
-
 using Landis.Core;
 
 namespace Landis.Extension.BaseHarvest
